736bf747b9a209c6a77e23e3b59e5973b971e0d6
mariadenise <maria.simoes@estudante.ifb.edu.br>
2021-06-01 20:54:36 -0300

    atualização de 2.2, 2.3 e 3.1

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    print('ID do perfil recebido: {}'.format(perfil_id))
+    return render(request, 'perfil.html')	
