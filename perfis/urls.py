0daa8a7ab07c774de38365bd57cc6cca4e05d5b5
mariadenise <maria.simoes@estudante.ifb.edu.br>
2021-06-23 17:33:59 -0300

    atualizaçao na pasta experiencein e perfis, experiencein/perfis/models.py para definicao do esquema do banco(criacao de um modelo)e geracao de esquemas baseados no modelo Perfil(models.Model) atraves do comando makemigrations que o Django utiliza para propagar as alteracoes e >>Python manage.py migrate, que efetiva as alteracoes geradas pelo comando >>>python manage.py makemigrations. Instanciado, alterado e executada a persistencia de um perfil, em seguida removida a instancia e utilizada a funcao view 'exibir', para buscar o perfil direto no banco(Utilização do Framework de Mapeamento de Objeto Relacional Django para persistir e recuperar objetos da classe 'Perfil').Criação de perfis com o método 'create', busca do perfil pelo 'id' e busca do id pelo 'nome',  busca do  perfil pelo nome e email, deleção de perfil e impressão do ID. Utilização do método filter para pesquisas.

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+	path('', views.index, name='index'),
+	path('perfis/<int:perfil_id>', views.exibir, name='exibir')
+	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -2,15 +2,10 @@ from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+	return render(request, 'index.html')
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+	# precisa agora buscar do banco
+    return render(request, 'perfil.html', { "perfil" : perfil})
