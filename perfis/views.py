92b8715a7e4e7e300383a7291fe70dd817019cf9
mariadenise <maria.simoes@estudante.ifb.edu.br>
2021-06-26 11:57:15 -0300

    nova tentativa de atualização Alterados os arquivos perfis/templates/index.html e perfis/views.py para construir uma 'li' (lista) dos perfis, renderizando a lista com template tag e tamplete de acesso às propriedades de objeto.

--- perfis/views.py
+++ perfis/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from perfis.models import Perfil
 
-def index(request):   #para trazer todos os perfis (uma lista se houver)
-    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
+def index(request):
+	return render(request, 'index.html', {'perfis': Perfil.objects.all()})
 
 def exibir(request, perfil_id):
-
-    perfil = Perfil.objects.get(id=perfil_id)
-	# precisa agora buscar do banco
-    return render(request, 'perfil.html', { "perfil" : perfil})
+	
+	perfil = Perfil.objects.get(id=perfil_id)
+	return render(request, 'perfil.html', {'perfil':perfil})
