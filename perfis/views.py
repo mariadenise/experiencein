5c7897c62ae423de14f966f744a2c8254171e385
mariadenise <maria.simoes@estudante.ifb.edu.br>
2021-06-29 09:20:02 -0300

    MODULO 8 - para exibir os convites recebidos foram adequados os arquivos perfis/template/index.html com o link aceitar para cada convite e o arquivo  perfil/views.py com o perfil_logado para simular o convite recebido.Enviados convites por meio do shell, então foi adequada o arquivo /perfis/templates/base.html para exibir o nome do perfil logado e o arquivo perfis/templates/index.html foi adequado com a inclusão das  variáveis with,  {{ perfil_logado.convites_recebidos.count }}  e também o filtro '|pluralize' para obter o total de convites de um perfil.

--- perfis/views.py
+++ perfis/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from perfis.models import Perfil
 
 def index(request):
-	return render(request, 'index.html', {'perfis': Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
-def exibir(request, perfil_id):	
-	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfil.html', {'perfil':perfil})
+def exibir(request, perfil_id):
+    # código comentado
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
