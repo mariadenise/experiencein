f2db8d16f66c406de91d5272c35a268d4ca541e5
mariadenise <maria.simoes@estudante.ifb.edu.br>
2021-06-28 22:14:33 -0300

    MODULO 6 - Alteração do arquivo experiencein/perfis/views.py, com uma função de view 'convidar' (vazia), com um 'pass' para não dar erro, a qual delegará para a método convidar, da classe Perfil, a responsabilidade de realizar um convite, incluindo o id (que será do convidado). Serão acessados os objetos que estão no Banco da classe Perfil que retornará o objeto completo do 'id' solicitado.Editado o arquivo experiencein/perfis/urls.py com a nova rota(3a. Rota do arquivo), também foi alterado o template experiencein/perfis/templates/perfil.html para exibir apenas o nome do perfil e um link para convidá-lo(arota será nomeada para reduzir as possíveis futuras alerações).Alterado o arquivo experiencein/perfis/models.py melhorando a classe Pefil com um método 'convidar', com a lógica responsável pelo convite de um perfil.Ajustado o arquivo  experiencein/perfis/templates/perfil.html, adicionado um botão 'âncora' que fornecerá o link para convite.Alterado o arquivo experiencein/perfis/models.py para criar um relacionamento entre o Perfil e os convidados e Alterado o arquivo experiencein/perfis/models.py implementando os campos 'solicitante' e 'convidado' na Classe convite.

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from . import views
+from perfis import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir')
+	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
 	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
+	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
 ]
